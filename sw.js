957916320370fdb6e007009dc53347c3ad7b8d7f
bayhaqdokumen-cyber <bayhaqdokumen@gmail.com>
2025-08-13 11:10:47 +0700

    Aplikasi 1

--- sw.js
+++ sw.js
@@ -1,27 +1,28 @@
-
-const CACHE = 'mht-cache-v1';
-const ASSETS = [
+const CACHE_NAME = 'menata-hari-tua-v1';
+const URLS = [
   './',
   './index.html',
-  './styles.css',
-  './app.js',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './manifest.json',
+  './assets/icon-192.png',
+  './assets/icon-512.png'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(URLS)));
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k!==CACHE).map(k => caches.delete(k))))
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => {
+    if (k !== CACHE_NAME) return caches.delete(k);
+  }))));
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(resp => resp || fetch(e.request).then(fresh => {
+      const copy = fresh.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, copy));
+      return fresh;
+    }).catch(() => caches.match('./index.html')))
   );
 });
